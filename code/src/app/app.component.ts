69500488e78fab52e5451037d9b0ca61cdba80a9
Vikram.R <Vikram.R@sysarcin.com>
2018-06-28 13:10:14 +0530

    changes made

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  arrValue = [];
+
+  constructor(){
+  }
+
+  ngOnInit(){
+    this.arrValue =   [
+      {fname: 'John',lname:'Doe',gender:'Male',email:'john@example.com',phone:'+91 123456789' },
+	  {fname: 'Mary',lname:'Moe',gender:'Female',email:'mary@example.com',phone:'+91 123456787' },
+	  {fname: 'July',lname:'July',gender:'Female',email:'july@example.com',phone:'+91 123456788' },
+	 
+ ];
+  }
+
 }
